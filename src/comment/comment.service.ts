e078a6068ee1c603edfe723e88bc4c10b506b938
Md. Humayun Kabir Sobuj <humayun506034@gmail.com>
2026-01-03 14:00:55 +0600

    redis caching added in service module

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -3,29 +3,37 @@ import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { sendResponse } from 'src/utils/sendResponse';
+import { CacheUtil } from 'src/cache/redis-cache.util';
 
 @Injectable()
 export class CommentService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly cacheUtil: CacheUtil,
+  ) {}
 
- async create(createCommentDto: any) {
-   try {
-     console.log(
-      '🚀 ~ CommentService ~ create ~ createCommentDto:',
-      createCommentDto,
-    );
+  async create(createCommentDto: any) {
+    try {
+      // console.log(
+      //   '🚀 ~ CommentService ~ create ~ createCommentDto:',
+      //   createCommentDto,
+      // );
 
-    const isBlogExist = await this.prisma.blog.findUnique({
-      where: { id: createCommentDto.blogId },
-    });
-    if (!isBlogExist) {
-      throw new BadRequestException('Blog not found');
+      const isBlogExist = await this.prisma.blog.findUnique({
+        where: { id: createCommentDto.blogId },
+      });
+      if (!isBlogExist) {
+        throw new BadRequestException('Blog not found');
+      }
+
+      const result = await this.prisma.comment.create({
+        data: createCommentDto,
+      });
+      await this.cacheUtil.deleteByPattern('blogs:list:*');
+
+      return sendResponse('Comment Added Successfully', result);
+    } catch (error) {
+      throw new BadRequestException(error);
     }
-
-    const result =await this.prisma.comment.create({ data: createCommentDto });
-    return sendResponse('Comment Added Successfully', result);
-   } catch (error) {
-    throw new BadRequestException(error);
-   }
   }
 }
