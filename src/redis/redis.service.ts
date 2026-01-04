9c7f38f4ab6a39278f6abd7d3b66408fb0e6b0f2
Md. Humayun Kabir Sobuj <humayun506034@gmail.com>
2026-01-04 11:24:24 +0600

    Modify

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,16 +1,24 @@
-
+import { ConfigService } from '@nestjs/config';
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import Redis from 'ioredis';
 
 @Injectable()
 export class RedisService implements OnModuleInit, OnModuleDestroy {
+  
+  constructor(private readonly configService: ConfigService) {}
   private client: Redis;
 
   onModuleInit() {
-    this.client = new Redis({
-      host: 'localhost', // Docker এ run করা host
-      port: 6379,        // default port
-    });
+    // this.client = new Redis({
+    //   host: 'localhost',
+    //   port: 6379,
+    // });
+    // this.client = new Redis(
+    //   'rediss://default:ASagAAImcDIwMDdkNmY0N2RmODI0YWZiOThiNjYzZDViNjI5MjYzNnAyOTg4OA@wired-sailfish-9888.upstash.io:6379',
+    // );
+    const redisUrl = this.configService.get<string>('REDIS_URL');
+
+    this.client = new Redis(redisUrl as string);
 
     this.client.on('connect', () => {
       console.log('Redis client connected');
